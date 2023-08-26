0024d3d4de4d994b60ea55a48b7049e1a383767a
Garrett Boatman <garrett@getrecord.com>
2023-08-26 15:20:46 -0700

    edited press

--- src/pages/press.js
+++ src/pages/press.js
@@ -19,8 +19,7 @@ const PressPage = ({path}) => (
         <li><a target={'_blank'} href="https://www.createastir.ca/articles/the-day-we-left-vsff">Refugee experience drives Bosnian drama "The Day We Left" at the Vancouver Short Film Festival</a></li>
         <li><a target={'_blank'} href="https://vimeo.com/666921597">The Day We Left - Global BC Interview with star Elma Begovic</a></li>
         <li><a target={'_blank'} href="https://filmcraziest.wordpress.com/2022/02/01/interview-actress-elma-begovic-on-her-short-film-the-day-we-left-horror-film-bite/">Actress Elma Begovic on Her Short Film “The Day We Left,” Horror Film “Bite”</a></li>
-        <li><a target={'_blank'} href="https://horrornews.net/141716/psychological-thriller-tear-us-apart-starring-elma-begovic-bite-makes-world-premiere/">Psychological Thriller Tear Us Apart Starring Elma Begovic (Bite) Makes World Premiere</a></li>
-        <li><a target={'_blank'} href="https://www.instagram.com/tv/CKMuZH1hpYA/?hl=en">Minute Shorts with Elma Begovic</a></li>
+        <li><a target={'_blank'} href="https://radiopublic.com/amigospc-GEvvdv/s1!a72e7">AmigosPodcast | Elma Begovic | The Day We Left</a></li>
         <li><a target={'_blank'} href="https://vimeo.com/540913135">Morning For All | Jutro Za Sve Sarajevo - Elma Begovic Interview</a></li>
         <li><a target={'_blank'} href="https://melrosetradingpost.org/cutie-pie-of-the-day-dogsofmtp-8/">Melrose Trading Post | JAN 13 CUTIE PIE OF THE DAY!</a></li>
       </ul>
